ffbdbd51c7fd6eb483c6a10c5e565b06c6ab57e3
Seiken <seikenkiyosu@icloud.com>
2015-08-29 23:23:45 +0900

    clean up

--- LooselyStabilizing_LE_simulator/src/LooselyStabilizing_LE_simulator_writngfile.java
+++ LooselyStabilizing_LE_simulator/src/LooselyStabilizing_LE_simulator_writngfile.java
@@ -13,13 +13,13 @@ import RandamPackage.RandomWay;
 class LooselyStabilizing_LE_simulator_writngfile{
 	public static final int Roundnum = 10000000;	//round数の上界
 
-	public static final int s = 192;		//nの上限(agent.timerの初期値)
+	public static final int s = 200;		//nの上限(agent.timerの初期値)
 
-	public static final int n_from = 30;	//nはn_from～n_toのデータをとる
+	public static final int n_from = 50;	//nはn_from～n_toのデータをとる
 	public static final int n_to = 64;		
 
-	public static final int Vset = 1;	//vの速度の上限
-	public static final int DistanceforInteraction = 1;	//interactionができる距離
+	public static final int Vset = 10;	//vの速度の上限
+	public static final int DistanceforInteraction = 5;	//interactionができる距離
 	public static final int Gridsize = 50;	//フィールドのサイズ(Gridsize*Gridsizeの正方形)
 	public static final int DataNum = 100;	//データ数
 
@@ -34,10 +34,7 @@ class LooselyStabilizing_LE_simulator_writngfile{
 
 	public static void main(String args[]){
 		Random random = new Random();
-		int CTcounter;
-		int CT, HT;
-		int SPperRound[] = new int[Roundnum];
-		
+
         File file = new File(WritingPath);
         if (!file.exists()) {
             System.out.println("ディレクトリが存在しません。");
@@ -45,59 +42,58 @@ class LooselyStabilizing_LE_simulator_writngfile{
         }
 
 		for(int n=n_from; n<=n_to; n++){
-
 			Agent agent[] = new Agent[n];
 			int CTsum=0, HTsum=0;
 			double CTave=0.0 , HTave=0.0;
 
 			/*DataNum個のDataの平均をファイルに保存する*/
 			for(int Data=0; Data < DataNum; Data++){
-
-				CTcounter = CT = HT = 0;
+				int CT = 0, HT = 0;
+				boolean HT_count_flag = false, CT_count_flag = true;
 
 				for(int i=0; i<n; i++)
 				agent[i] = new Agent(random.nextBoolean(), s, random.nextInt(Gridsize)+random.nextDouble(), random.nextInt(Gridsize)+random.nextDouble());
 
 				for(int i=0; i<Roundnum; i++){
-						int leadercount=0;
-						int leaderid=0;
-						for(int j=0; j<n; j++) if(agent[j].IsLeader()){ leaderid = j; leadercount++; }
-						if(leadercount==1){
-								SPperRound[i] = leaderid;
-								if(i>0)//はじめからリーダが１個だったときのため
-								if(SPperRound[i-1]==leaderid){
-									HT++;
-								}
-								else{
-									HT = 0; CT = CTcounter;
-//									System.out.println(leaderid); 	//リーダのidを返す
-								}
+					int leadercount=0;
+		//			int leaderid=0;
+					//リーダの数をかぞえる
+					for(int j=0; j<n; j++) if(agent[j].IsLeader()){ leadercount++; }
+					
+					//Holding Timeが終了したらぬける
+					if(leadercount!=1 && HT_count_flag==true){ break; }
+					
+					if(leadercount==1){ 
+								HT_count_flag = true;
+								CT_count_flag = false;
 							}
-//						System.out.println("the number of leaders = " + leadercount);
-						int p, q;
-						while(true){					//リーダが決定するまで
-							p = random.nextInt(n);		//interactionをするagentをランダムで選択
-							q = RandomWay.RandamPickNearAgent( p, n, agent, DistanceforInteraction);		//p�Ƌ���1�ȓ��ɂ���m�[�h�̒���(���id�̒Ⴂ)�m�[�h��q�ɑ��
-							if(q != -1) { 	//pの周りにinteractionが可能なAgentが見つかったとき
-								Interaction.interaction(agent[p], agent[q], s);	
-								for(int j=0; j<n; j++) agent[j].Countdown();	//交流したagentのtimerをデクリメント
-								break;						//次のラウンドへ
-							}
-							for(int j=0; j<n; j++){					//各Agentをランダムに移動させる
-								agent[j].Vchange((random.nextInt(2*Vset)-Vset)+random.nextDouble() , (random.nextInt(2*Vset)-Vset)+random.nextDouble() );
-								agent[j].ShiftPointForTorus(Gridsize);	//移動
-							}
+					
+					if(HT_count_flag==true) HT++;
+				
+					int p, q;
+					while(true){					//リーダが決定するまで
+						p = random.nextInt(n);		//interactionをするagentをランダムで選択
+						q = RandomWay.RandamPickNearAgent( p, n, agent, DistanceforInteraction);		//p�Ƌ���1�ȓ��ɂ���m�[�h�̒���(���id�̒Ⴂ)�m�[�h��q�ɑ��
+						if(q != -1) { 	//pの周りにinteractionが可能なAgentが見つかったとき
+							Interaction.interaction(agent[p], agent[q], s);	
+							for(int j=0; j<n; j++) agent[j].Countdown();	//交流したagentのtimerをデクリメント
+							break;						//次のラウンドへ
 						}
-						CTcounter++;
-						if(CT!=0 && HT!=0 && HT+CT+1 < CTcounter) break;	//リーダが決まった瞬間処理を抜けたかった
+						for(int j=0; j<n; j++){					//各Agentをランダムに移動させる
+							agent[j].Vchange((random.nextInt(2*Vset)-Vset)+random.nextDouble() , (random.nextInt(2*Vset)-Vset)+random.nextDouble() );
+							agent[j].ShiftPointForTorus(Gridsize);	//移動
+						}
+					}
+					if(CT_count_flag==true) CT++;
 					}
 
 
 				   CTsum += CT;
 				   HTsum += HT;
-					System.out.println("(n:" + n_from + "~" + n_to + "," + "Datanum:" + DataNum + ")"
-							+ "\tn = " + n
-							+ ",\tData number = " + (Data+1)
+					System.out.println("( " + RandomMethod + " " + CoodinateSystem + " " + Vset + "V_" + DistanceforInteraction + "DI_" + Gridsize + "GS_" + DataNum + "DN"
+							+ "  s:" + s +  ", n:" + n_from + "~" + n_to + " )"
+							+ "\t n = " + n
+							+ ", Data number = " + (Data+1)
 							+ ",\tCT = " + CT + ",\tHT = " + HT);
 			}	//for(int Data=0; Data < DataNum; Data++) 終了
 			CTave = (double)CTsum / DataNum;
